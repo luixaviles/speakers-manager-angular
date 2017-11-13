47b3c87abb4157920bf1d27c626ee7ebb5eb9718
luixaviles <luis.avilesz@gmail.com>
2017-11-13 00:46:19 -0400

    Adding angular material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
